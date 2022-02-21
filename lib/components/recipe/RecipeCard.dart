6e9146e17b2e228c671643911dffda8a905d248c
arie0703 <[ari0703sophia@gmail.com]>
2022-02-22 08:57:09 +0900

    レシピ一覧のサムネイルUI作成

--- lib/components/recipe/RecipeCard.dart
+++ lib/components/recipe/RecipeCard.dart
@@ -1,27 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:curry_app/CustomClass.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class RecipeCard extends StatelessWidget {
-  const RecipeCard({Key? key}) : super(key: key);
+class RecipeCard extends StatefulWidget {
+  const RecipeCard({Key? key, required this.data, required this.docID})
+      : super(key: key);
+  final Map data;
+  final String docID;
+  @override
+  _RecipeCardState createState() => _RecipeCardState();
+}
+
+class _RecipeCardState extends State<RecipeCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: CommonColor.primaryColor[50],
-      child: Column(
-        children: [
-          const ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Sample Curry'),
-            subtitle: Text('India Curry'),
-          ),
-          Container(
-              alignment: Alignment.center,
-              child: Image.asset('assets/sample.jpg')),
-          Column(
-            children: [Text("カレールー"), Text("ご飯"), Text("福神漬け")],
-          ),
-        ],
-      ),
-    );
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .doc(widget.data['user_id'])
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: SizedBox(),
+            );
+          }
+          if (snapshot.hasError) {
+            //
+            return const Text('Something went wrong');
+          }
+          return GestureDetector(
+            onTap: () {},
+            child: Card(
+                color: CommonColor.primaryColor[50],
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 120,
+                        padding: EdgeInsets.only(right: 10),
+                        child: widget.data["imageURL"] != null
+                            ? Image.network(widget.data["imageURL"])
+                            : Image.asset('assets/noimage.png',
+                                fit: BoxFit.cover),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(widget.data["title"],
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: CommonColor.primaryColor[800])),
+                          Text("by " + snapshot.data['name'],
+                              style: TextStyle(height: 1.5)),
+                          Text(widget.data['ingredients'].join(","),
+                              style: TextStyle(height: 2.0))
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
+          );
+        });
   }
 }
